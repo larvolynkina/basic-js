fa5d36dda98b74524c0f25e0e34b70730b1bf288
Lara <volynkinala@gmail.com>
2022-09-01 11:18:11 +0400

    feat: implement carbon-dating and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,32 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity === 'string' &&
+    Number.isFinite(Number(sampleActivity)) &&
+    Number(sampleActivity) > 0 &&
+    Number(sampleActivity) <= 15
+  ) {
+    return Math.ceil(
+      HALF_LIFE_PERIOD /
+        (Math.log10(2) / Math.log10(MODERN_ACTIVITY / Number(sampleActivity)))
+    );
+  }
+  return false;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
